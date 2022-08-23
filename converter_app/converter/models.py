5e853d49170ecb9d1dcc50554d499997c83f8154
Regiane <nascire@gmail.com>
2022-08-22 21:13:19 -0300

    create models and test them.

--- converter_app/converter/models.py
+++ converter_app/converter/models.py
@@ -1,2 +1,26 @@
 from django.db import models
 
+
+class Product(models.Model):
+    """
+    The Product object contains the name of a registered product.
+    """
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Prices(models.Model):
+    """
+    The Prices object contains all prices in each currency for a registered product.
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='prices')
+    slug_code = models.SlugField()
+    currency_country = models.CharField(max_length=255)
+    value = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return self.slug_code
+
---
+++ converter_app/tests/test_models.py
@@ -0,0 +1,74 @@
+from decimal import Decimal
+
+import pytest
+from converter.models import Product, Prices
+from django.db import IntegrityError
+
+
+@pytest.fixture
+def product_payload():
+    return {
+            "name": "Tenis_nike'",
+            "description": "Tenis feminino preto e rosa",
+        }
+
+
+@pytest.fixture
+def prices_payload():
+    return [
+                {
+                    "slug_code": "BRL",
+                    "value": "529.29",
+                    "currency_country": "Brazil"
+                },
+                {
+                    "slug_code": "USD",
+                    "value": "98.23",
+                    "currency_country": "United States"
+                },
+                {
+                    "slug_code": "EUR",
+                    "value": "83.26",
+                    "currency_country": "Countries in Europe"
+                },
+                {
+                    "slug_code": "INR",
+                    "value": "7318.93",
+                    "currency_country": "India"
+                }
+            ]
+
+
+@pytest.mark.django_db
+def test_create_product(product_payload):
+    """
+    Create a Product instance from a payload.
+    """
+    product = Product.objects.create(**product_payload)
+    assert product.name == product_payload['name']
+    assert product.description == product_payload['description']
+
+
+@pytest.mark.django_db
+def test_not_create_product_already_exist(product_payload):
+    """
+    Raise an Integrity error when try to create a Product that already exist.
+    """
+    Product.objects.create(**product_payload)
+    with pytest.raises(IntegrityError):
+        Product.objects.create(**product_payload)
+
+
+@pytest.mark.django_db
+def test_create_prices(prices_payload, product_payload):
+    """
+    Create a Prices instance from a payload.
+    """
+    product = Product.objects.create(**product_payload)
+    for price in prices_payload:
+        Prices.objects.create(product=product, **price)
+
+    for i, price in enumerate(product.prices.all()):
+        assert price.slug_code == prices_payload[i]['slug_code']
+        assert price.value == Decimal(prices_payload[i]['value'])
+        assert price.currency_country == prices_payload[i]['currency_country']
