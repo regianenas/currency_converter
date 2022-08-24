7ad5138e35522aad23cbba9d3cfe3d0babaedf45
Regiane <nascire@gmail.com>
2022-08-24 15:57:49 -0300

    add logs in veiws.

--- converter_app/converter/views.py
+++ converter_app/converter/views.py
@@ -5,6 +5,9 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.pagination import PageNumberPagination
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -18,7 +21,7 @@ def get_update_or_delete_prices_per_product(
     try:
         product = Product.objects.get(name=name)
     except Product.DoesNotExist:
-        # put a log here
+        logger.error(f"{name} is not registered in database.")
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
@@ -29,11 +32,14 @@ def get_update_or_delete_prices_per_product(
         serializer = ProductSerializer(product, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
+            logger.info(f"Product {name} was updated:{request.data}")
             return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+        logger.error(f"Error when try to register product {serializer.errors}.")
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
         product.delete()
+        logger.info(f"Product {name} was deleted from database.")
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
@@ -54,6 +60,7 @@ def get_create_product(request: Request) -> Response:
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            logger.info(f"New product was registered {request.data}")
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-
+        logger.error(f"Error when try to register product {serializer.errors}.")
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
